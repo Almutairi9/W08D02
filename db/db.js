19a580719aaf6d7a3d1d4523160082d7d00a3ca6
Rawan Almutairi <rawan.fahad@tuwaiq.edu.sa>
2021-11-29 22:19:44 +0300

    Bulid user router

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 const DB = process.env.DB;
 const options = {
   useNewUrlParser: true,
--- routers/routes/role.js
+++ routers/routes/role.js
@@ -1,31 +1,11 @@
-const roleModel = require("../../db/models/role");
+const express = require("express");
+const { createRole, roles } = require("./../controller/role");
 
-const create = (req, res) => {
-  const { role, permission } = req.body;
-  const newRole = new roleModel({
-    role,
-    permission,
-  });
-  newRole
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-};
-const getRoles = (req, res) => {
-  roleModel
-    .find({})
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-};
-module.exports = {
-  create,
-  getRoles,
-};
+const roleRouter = express.Router();
+
+
+roleRouter.post("/createrole", createRole);
+roleRouter.get("/roles", roles);
+
+
+module.exports = roleRouter;
